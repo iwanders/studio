4e4ec5102d35080e2a821b80326e867dd671656d
Ivor Wanders <ivor@iwanders.net>
2021-11-12 15:40:47 -0500

    Change publicPath to "auto".
    This allows hosting the files from any directory as opposed to just from the root.

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -97,7 +97,7 @@ const mainConfig = (env: unknown, argv: WebpackArgv): Configuration => {
     devtool: isDev ? "eval-cheap-module-source-map" : "source-map",
 
     output: {
-      publicPath: "/",
+      publicPath: "auto",
 
       // Output filenames should include content hashes in order to cache bust when new versions are available
       filename: isDev ? "[name].js" : "[name].[contenthash].js",
